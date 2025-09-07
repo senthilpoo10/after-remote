8f89acf43366dc557071e1e14f1f011a837f0256
senthilpoo10 <senthilpoo10@gmail.com>
2025-09-07 17:49:13 +0300

    Delete frontend/src directory

--- frontend/src/components/general/index.ts
+++
@@ -1,7 +0,0 @@
-// Individual components
-export { Alert } from './Alert';
-export { ErrorBoundary } from './ErrorBoundary';
-export { Layout } from './Layout';
-export { LoadingSpinner } from './LoadingSpinner';
-export { Navbar } from './Navbar';
-export { QRCode } from './QRCode';
